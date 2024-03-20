3b885bc32b08275891890e9b268b6881bd689947
TomTzadka <tomtzadka@gmail.com>
2024-03-20 15:14:23 +0200

    add get and remove service

--- server.js
+++ server.js
@@ -1,5 +1,74 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+
+import { loggerService } from './services/logger.service.js'
+import { bugService } from "./services/bugs.service.js"
 
 const app = express()
-app.get('/', (req, res) => res.send('Hello there'))
-app.listen(3030, () => console.log('Server ready at port 3030'))
+// Express Config:
+// app.use(express.static('public'))
+app.use(cookieParser())
+
+app.get('/', (req, res) => res.send('Hello there Tom!'))
+
+
+app.get('/api/bugs', (req, res) => {
+    console.log('get');
+    bugService.query()
+        .then(bugs => {
+            res.send(bugs)
+        })
+        .catch(err => {
+            loggerService.error('Cannot get bugs', err)
+            res.status(400).send('Cannot get bugs')
+        })
+})
+
+app.get('/api/bugs/save', (req, res) => {
+    return new Promise(res.send('save'))
+    
+    const bugToSave = {
+        vendor: req.query.vendor,
+        speed: +req.query.speed,
+        desc: req.query.desc,
+        _id: req.query._id
+    }
+    // res.send(req.query)
+    // bugService.save(bugToSave)
+    //     .then(bug => res.send(bug))
+    //     .catch((err) => {
+    //         loggerService.error('Cannot save bug', err)
+    //         res.status(400).send('Cannot save bug')
+    //     })
+})
+
+
+
+
+app.get('/api/bugs/:id', (req, res) => {
+    const bugId = req.params.id
+    bugService.getById(bugId)
+        .then(bug => res.send(bug))
+        .catch(err => {
+            loggerService.error(err)
+            res.status(400).send('Cannot get bug')
+        })
+})
+
+app.get('/api/bugs/:id/remove', (req, res) => {
+    console.log('delete....');
+    const bugId = req.params.id
+    console.log('req.params',req.params);
+    bugService.remove(bugId)
+        .then(() => res.send(bugId))
+        .catch((err) => {
+            loggerService.error('Cannot remove bug', err)
+            res.status(400).send('Cannot remove bug')
+        })
+})
+
+
+
+
+const port = 3030
+app.listen(port, () => console.log('Server ready at port 3030'))
---
+++ services/bugs.service.js
@@ -0,0 +1,56 @@
+import fs from 'fs'
+import { utilService } from "./utils.service.js";
+import { resolve } from 'path';
+
+
+export const bugService = {
+    query,
+    getById,
+    remove,
+    save
+}
+
+const bugs = utilService.readJsonFile('./data/bugs.json')
+
+function query() {
+    return Promise.resolve(bugs)
+}
+
+function getById(id) {
+    const bug = bugs.find(bug => bug._id === id)
+    if (!bug) return Promise.reject('bug does not exist!')
+    return Promise.resolve(bug)
+}
+
+function remove(id) {
+    const bugIdx = bugs.findIndex(bug => bug._id === id)
+    bugs.splice(bugIdx, 1)
+    return _saveBugsToFile()
+
+}
+
+function save(bug) {
+    if (bug._id) {
+        const bugIdx = bugs.findIndex(_bug => _bug._id === bug._id)
+        bugs[bugIdx] = bug
+    } else {
+        bug._id = utilService.makeId()
+        bugs.unshift(bug)
+    }
+    return _saveBugsToFile().then(() => bug)
+}
+
+
+function _saveBugsToFile() {
+    // return new Promise(resolve => resolve('saveBugsToFile'))
+    return new Promise((resolve, reject) => {
+        const data = JSON.stringify(bugs, null, 4)
+        fs.writeFile('./data/bugs.json', data, (err) => {
+            if (err) {
+                console.log(err)
+                return reject(err)
+            }
+            resolve()
+        })
+    })
+}
